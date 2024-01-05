8838662c069aad98b1d2f451f41e9b1afa0dcc53
alexisNorthCoders <alexiscreoulo@hotmail.com>
2024-01-05 23:52:33 +0000

    removed twilio and added whatsapp-web.js

--- index.js
+++ index.js
@@ -2,25 +2,98 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const twilio = require('twilio');
 const { OpenAI } = require('openai');
-const dotenv = require("dotenv").config()
-const fetch = require('node-fetch')
-const path = require('path')
+const dotenv = require("dotenv").config();
+const fetch = require('node-fetch');
+const path = require('path');
 
+const qrcode = require('qrcode-terminal');
+
+const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
+
+const client = new Client({ authStrategy: new LocalAuth() });
 const app = express();
-const port = process.env.PORT
-app.use(express.json())
-app.use(express.static(path.join(__dirname, 'public')));
+const port = process.env.PORT;
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Initialize Twilio client
-const twilioClient = new twilio(process.env.TWILIO_TOKEN1, process.env.TWILIO_TOKEN2);
+client.on('qr', qr => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+  console.log('Whatsapp Initiated!');
+});
+
+client.on('message', async message => {
+  console.log(message.body);
+  try {
+
+    if (message.body.startsWith("Gpt3")) {
+      const prompt = message.body.replace("Gpt3 ", "");
+      const response = await gPT3generateResponse(prompt);
+      client.sendMessage(message.from, response);
+    }
+    else if (message.body.startsWith("Gpt3Instruct")) {
+      const prompt = message.body.replace("GptInstruct ", "");
+      const response = await instructGenerateResponse(prompt);
+      client.sendMessage(message.from, response);
+    }
+    else if (message.body.startsWith("Gpt4")) {
+      const prompt = message.body.replace("Gpt4 ", "");
+      const response = await gPT4generateResponse(prompt);
+      client.sendMessage(message.from, response);
+    }
+    else if (message.body.startsWith("Wizard")) {
+      const prompt = message.body.replace("Wizard ", "");
+      const response = await gPT3WizardgenerateResponse(prompt);
+      client.sendMessage(message.from, response);
+    }
+    else if (message.body.startsWith("Recipe")) {
+      const prompt = message.body.replace("Recipe ", "");
+      const response = await recipeGenerateResponse(prompt)
+      client.sendMessage(message.from, response);
+    }
+    else if (message.body.startsWith("Weather")) {
+      const prompt = message.body.replace("Weather ", "");
+      const forecast = await getWeatherData(prompt)
+      if (JSON.stringify(forecast).includes("not found")) {
+        client.sendMessage(message.from, `${city} city not found!`);
+      }
+      else{
+        const weather = `forecast: ${forecast.weather[0].description}\ntemperature:${forecast.main.temp}\nmax:${forecast.main.temp_max}\nmin:${forecast.main.temp_min}`;
+        client.sendMessage(message.from, weather);
+      }
+    }
+    else if (message.body === '!Daniel') {
+      const media = MessageMedia.fromFilePath('./files/photo001.jpg');
+      client.sendMessage(message.from, media, { caption: 'Foto do Daniel' });
+    }
+    else if (message.body.startsWith("Dalle")) {
+      const prompt = message.body.replace("Dalle ", "");
+      const response = await dallegenerateResponse(prompt);
+      const media = await MessageMedia.fromUrl(response);
+      client.sendMessage(message.from, media);
+    }
+    else if (message.body.startsWith("Help")){
+      client.sendMessage(message.from, "Try any of my commands: \nWizard \nGpt3 \nDalle\nRecipe \nWeather")
+    }
+    else{ 
+      const prompt = message.body
+      const response = await assistantgenerateResponse(prompt);
+      client.sendMessage(message.from, response);
+    }
+  }
+  catch (err) { console.log(err); }
+});
+
+client.initialize();
 
 // Initialize OpenAI API client
-const openai = new OpenAI({apiKey:process.env.OPENAI_API_KEY});
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-// Function to fetch weather data
+
 async function getWeatherData(city) {
-  const apiKey = process.env.OPENWEATHER_API_KEY
+  const apiKey = process.env.OPENWEATHER_API_KEY;
   const weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`;
   try {
     const response = await fetch(weatherURL);
@@ -31,17 +104,35 @@ async function getWeatherData(city) {
     throw error;
   }
 }
-// Function to generate a response from ChatGPT
+
+async function assistantgenerateResponse(userMessage) {
+  try {
+    const completion = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo-1106',
+      messages: [
+        {"role": "system","content":"You are a helpful assistant. You are inside a whatsapp conversation. You have many capabilities. You can tell the weather, send random pictures of Daniel, tell jokes and so on."},
+        { "role": "user", "content": userMessage }],
+      max_tokens: 1000
+      // Adjust this as needed for desired response length
+    });
+
+
+    return completion.choices[0].message.content.trim();
+  } catch (error) {
+    console.error('Error generating GPT-3 response:', error);
+    throw new Error('Error generating response');
+  }
+}
 async function gPT3generateResponse(userMessage) {
   try {
     const completion = await openai.chat.completions.create({
       model: 'gpt-3.5-turbo-1106',
-      messages:[{"role":"user", "content": userMessage}],
-      max_tokens:1000
-       // Adjust this as needed for desired response length
+      messages: [{ "role": "user", "content": userMessage }],
+      max_tokens: 1000
+      // Adjust this as needed for desired response length
     });
-    
-    
+
+
     return completion.choices[0].message.content.trim();
   } catch (error) {
     console.error('Error generating GPT-3 response:', error);
@@ -53,14 +144,14 @@ async function gPT4generateResponse(userMessage) {
     const completion = await openai.chat.completions.create({
       model: 'gpt-4',
       temperature: 0.7,
-      messages:[
-        {"role":"system", "content": "You are a helpful javascript assistant. When given code you will refactor according to instructions."},
-      {"role":"user", "content": userMessage}],
-      max_tokens:1000
-       // Adjust this as needed for desired response length
+      messages: [
+        { "role": "system", "content": "You are a helpful javascript assistant. When given code you will refactor according to instructions." },
+        { "role": "user", "content": userMessage }],
+      max_tokens: 1000
+      // Adjust this as needed for desired response length
     });
-    
-    
+
+
     return completion.choices[0].message.content.trim();
   } catch (error) {
     console.error('Error generating GPT-3 response:', error);
@@ -71,13 +162,13 @@ async function dallegenerateResponse(userMessage) {
   try {
     const response = await openai.images.generate({
       model: "dall-e-2",
-      prompt:  userMessage,
+      prompt: userMessage,
       n: 1,
       size: "1024x1024",
     });
-    imageUrl = response.data[0].url
-    console.log(imageUrl)
-     
+    imageUrl = response.data[0].url;
+    console.log(imageUrl);
+
     return imageUrl;
   } catch (error) {
     console.error('Error generating Dall-e response:', error);
@@ -89,10 +180,10 @@ async function recipeGenerateResponse(userMessage) {
     const completion = await openai.completions.create({
       model: 'gpt-3.5-turbo-instruct',
       temperature: 0.7,
-      prompt:`create a detailed recipe with only the following ingredients: ${userMessage}. Start off with 'recipe name', 'ingredients list' and 'step-by-step procedure for cooking`,
-      max_tokens:1000
+      prompt: `create a detailed recipe with only the following ingredients: ${userMessage}. Start off with 'recipe name', 'ingredients list' and 'step-by-step procedure for cooking`,
+      max_tokens: 1000
     });
-    
+
     return completion.choices[0].text.trim();
   } catch (error) {
     console.error('Error generating GPT-3 response:', error);
@@ -104,52 +195,58 @@ async function instructGenerateResponse(userMessage) {
     const completion = await openai.completions.create({
       model: 'gpt-3.5-turbo-instruct',
       temperature: 0.7,
-      prompt:userMessage,
-      max_tokens:1400
+      prompt: userMessage,
+      max_tokens: 1400
     });
-    
+
     return completion.choices[0].text.trim();
   } catch (error) {
     console.error('Error generating GPT-3 response:', error);
     throw new Error('Error generating response');
   }
 }
-
-// Function to send a message via Twilio
-async function sendMessage(to, message) {
+async function gPT3WizardgenerateResponse(userMessage) {
   try {
-    await twilioClient.messages.create({
-      body: message,
-      from: 'whatsapp:+14155238886', // Twilio phone number
-      to,
+    const completion = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo-1106',
+      messages: [{"role":"system","content":"You are a wizard. You speak like an eclectic wizard and in riddles. If someone asks your name is Isildor The Great. You always finish your conversation in a form of a wise advice."},
+        { "role": "user", "content": userMessage }],
+      max_tokens: 1000
+      // Adjust this as needed for desired response length
     });
+
+
+    return completion.choices[0].message.content.trim();
   } catch (error) {
-    console.error('Error sending message:', error);
-    throw new Error('Error sending message');
+    console.error('Error generating GPT-3 response:', error);
+    throw new Error('Error generating response');
   }
 }
 
 app.post('/weather', async function (req, res) {
-  
+
   let city = await req.body.city.trim();
-  console.log(city)
-  const forecast = await getWeatherData(city)
-  if (JSON.stringify(forecast).includes("not found")){
-    res.status(404).send(`${city} city not found!`)
+  console.log(city);
+  const forecast = await getWeatherData(city);
+  if (JSON.stringify(forecast).includes("not found")) {
+    res.status(404).send(`${city} city not found!`);
   }
-  else{
-  res.send({forecast: forecast.weather[0].description,
-    temp:forecast.main.temp,
-  max:forecast.main.temp_max,
-min:forecast.main.temp_min
-})}})
+  else {
+    res.send({
+      forecast: forecast.weather[0].description,
+      temp: forecast.main.temp,
+      max: forecast.main.temp_max,
+      min: forecast.main.temp_min
+    });
+  }
+});
 app.post('/gpt3', async (req, res) => {
   const userMessage = req.body.userPrompt;
-  
-    try {
-    
+
+  try {
+
     const response = await gPT3generateResponse(userMessage);
-      
+
     res.send(response);
   } catch (error) {
     console.error('Error:', error);
@@ -158,11 +255,11 @@ app.post('/gpt3', async (req, res) => {
 });
 app.post('/gpt4', async (req, res) => {
   const userMessage = req.body.userPrompt;
-  
-    try {
-    
+
+  try {
+
     const response = await gPT4generateResponse(userMessage);
-      
+
     res.send(response);
   } catch (error) {
     console.error('Error:', error);
@@ -171,110 +268,110 @@ app.post('/gpt4', async (req, res) => {
 });
 app.post('/dalle2', async (req, res) => {
   const userMessage = req.body.userPrompt;
-  
-    try {
-    
+
+  try {
+
     const response = await dallegenerateResponse(userMessage);
-      
+
     res.send(response);
   } catch (error) {
     console.error('Error:', error);
     res.status(500).send('Error processing message');
   }
 });
-app.post('/instruct',async (req, res) => {
+app.post('/instruct', async (req, res) => {
   const userMessage = req.body.userPrompt;
-  console.log(userMessage)
+  console.log(userMessage);
   try {
     const response = await instructGenerateResponse(userMessage);
-    
-  res.send(response);
-} catch (error) {
-  console.error('Error:', error);
-  res.status(500).send('Error processing message');
-}
-})
-app.post('/recipe',async (req, res) => {
-  const userMessage = req.body.userPrompt;
-  console.log(userMessage)
-  try {
-    const response = await recipeGenerateResponse(userMessage);
-    
-  res.send(response);
-} catch (error) {
-  console.error('Error:', error);
-  res.status(500).send('Error processing message');
-}
-})
-app.post('/whatsapp', async (req, res) => {
 
-  const userMessage = req.body.Body
-  console.log(userMessage)
-if (userMessage.startsWith("Dalle")){
-  const prompt = userMessage.replace("Dalle","")
-  try {
-  const response = await dallegenerateResponse(prompt);
-   // Send the response back to the user via Twilio
-   await sendMessage(req.body.From, response);
-   res.send(response);
-}
-catch (error) {
-  console.error('Error:', error);
-  res.status(500).send('Error processing message');
-}}
-if (userMessage.startsWith("Weather")){
-  const prompt = userMessage.replace("Weather ","")
-  try {
-    // Process the user's message using ChatGPT
-    const forecast = await getWeatherData(prompt)
-    
-    if (JSON.stringify(forecast).includes("not found")){
-      res.status(404).send(`${city} city not found!`)
-    }
-    else{
-    const weather = `forecast: ${forecast.weather[0].description}\ntemperature:${forecast.main.temp}\nmax:${forecast.main.temp_max}\nmin:${forecast.main.temp_min}`
-    
-    console.log(forecast)
-    // Send the response back to the user via Twilio
-    await sendMessage(req.body.From, weather);
-  
-    res.send(weather)}
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send('Error processing message');
-  }}
-if (userMessage.startsWith("Gpt3")){
-  const prompt = userMessage.replace("Gpt3","")
-try {
-  // Process the user's message using ChatGPT
-  const response = await gPT3generateResponse(prompt);
-  
-  // Send the response back to the user via Twilio
-  await sendMessage(req.body.From, response);
-
-  res.send(response);
-} catch (error) {
-  console.error('Error:', error);
-  res.status(500).send('Error processing message');
-}}
-if (userMessage.startsWith("Gpt4")){
-  const prompt = userMessage.replace("Gpt4","")
-  try {
-    // Process the user's message using ChatGPT
-    const response = await gPT4generateResponse(prompt);
-    
-    // Send the response back to the user via Twilio
-    await sendMessage(req.body.From, response);
-  
     res.send(response);
   } catch (error) {
     console.error('Error:', error);
     res.status(500).send('Error processing message');
-  }}
+  }
+});
+app.post('/recipe', async (req, res) => {
+  const userMessage = req.body.userPrompt;
+  console.log(userMessage);
+  try {
+    const response = await recipeGenerateResponse(userMessage);
+
+    res.send(response);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Error processing message');
+  }
+});
+app.post('/whatsapp?:number', async (req, res) => {
+  const { number } = req.query;
+  const userMessage = req.body.Body;
+  console.log(userMessage);
+  if (userMessage.startsWith("Dalle")) {
+    const prompt = userMessage.replace("Dalle", "");
+    try {
+      const response = await dallegenerateResponse(prompt);
+      client.sendMessage(`${number}@c.us`, response);
+      res.send(response);
+    }
+    catch (error) {
+      console.error('Error:', error);
+      res.status(500).send('Error processing message');
+    }
+  }
+  if (userMessage.startsWith("Weather")) {
+    const prompt = userMessage.replace("Weather ", "");
+    try {
+      const forecast = await getWeatherData(prompt);
+
+      if (JSON.stringify(forecast).includes("not found")) {
+        res.status(404).send(`${city} city not found!`);
+      }
+      else {
+        const weather = `forecast: ${forecast.weather[0].description}\ntemperature:${forecast.main.temp}\nmax:${forecast.main.temp_max}\nmin:${forecast.main.temp_min}`;
+
+        console.log(forecast);
+       
+        await client.sendMessage(`${number}@c.us`, weather);
+
+        res.send(weather);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).send('Error processing message');
+    }
+  }
+  if (userMessage.startsWith("Gpt3")) {
+    const prompt = userMessage.replace("Gpt3", "");
+    try {
+      // Process the user's message using ChatGPT
+      const response = await gPT3generateResponse(prompt);
+      client.sendMessage(`${number}@c.us`, response);
+
+      res.send(response);
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).send('Error processing message');
+    }
+  }
+  if (userMessage.startsWith("Gpt4")) {
+    const prompt = userMessage.replace("Gpt4", "");
+    try {
+      // Process the user's message using ChatGPT
+      const response = await gPT4generateResponse(prompt);
+
+      await client.sendMessage(`${number}@c.us`, response);
+
+      res.send(response);
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).send('Error processing message');
+    }
+  }
 });
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
-})
+});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
