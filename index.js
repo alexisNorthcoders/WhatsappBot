09aa9350d9e1cc454df4cc8f490633b95c58d185
alexisNorthCoders <alexiscreoulo@hotmail.com>
2024-02-06 00:18:03 +0000

    change react to test

--- index.js
+++ index.js
@@ -317,10 +317,10 @@ app.get('/', (req, res) => {
 
 });
 
-app.use(express.static(path.join(__dirname, '/ChatAI/')));
+app.use(express.static(path.join(__dirname, '/ChatAI/dist')));
 
 app.get('/chat', (req, res) => {
-  res.sendFile(path.join(__dirname, '/ChatAI/', 'index.html'));
+  res.sendFile(path.join(__dirname, '/ChatAI/dist', 'index.html'));
 });
 
 app.listen(port,'0.0.0.0', () => {
