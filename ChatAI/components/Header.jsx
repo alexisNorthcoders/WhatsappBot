568368cb8c119dc6d2f970ff3cc4d05a1857b600
alexisNorthCoders <alexiscreoulo@hotmail.com>
2024-02-08 00:13:47 +0000

    change path of image

--- ChatAI/components/Header.jsx
+++ ChatAI/components/Header.jsx
@@ -14,7 +14,7 @@ export default function Header() {
       <Col >
         <Image
           id="avatar"
-          src="../src/assets/pi.jpeg"
+          src="../dist/assets/pi.jpeg"
           width="100px"
           alt="avatar image"
         />
